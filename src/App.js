45dcbe741be7ab59633426635906f2e1ae3b6c54
Sean Michael Shea <seanshea1337@gmail.com>
2021-06-06 11:15:01 -0400

    remove log from app and map through people array

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import data from "./data";
 import List from "./List";
 function App() {
 	const [people, setPeople] = useState(data);
-	console.log("this is people", people);
-
 	return (
 		<main>
 			<section className="container">
