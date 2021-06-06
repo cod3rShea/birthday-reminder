04380310a97c6204b16d5ae001cf2a443e6b5bf5
Sean Michael Shea <seanshea1337@gmail.com>
2021-06-06 11:26:34 -0400

    create a function to clear all users

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import data from "./data";
 import List from "./List";
 function App() {
 	const [people, setPeople] = useState(data);
+
+	const clearList = () => {
+		setPeople([]);
+	};
+
 	return (
 		<main>
 			<section className="container">
 				<h3>{people.length} Birthdays today</h3>
 				<List people={people} />
+				<button onClick={clearList}>Clear List</button>
 			</section>
 		</main>
 	);
